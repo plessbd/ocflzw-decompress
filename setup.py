0835b8c371ab3b225c12cb11914d59d61bf5404f
Ben Plessinger <Benjamin.Plessinger@roswellpark.org>
2022-04-27 14:08:08 -0400

    PyPI prep

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import ocflzw_decompress
 
-with open("README.md", "r") as fh:
+with open("readme.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='ocflzw-decompress',
-    version=ocflzw_decompress.VERSION,
+    version="1.0.0",
     description='OCFLZW Decompress',
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/plessbd/ocflzw-decompress",
+    project_urls={
+        "Bug Tracker": "https://github.com/plessbd/ocflzw-decompress/issues",
+    },
     keywords='lzw ocfblob decompress cerner',
     author='Ben Plessinger, Mohammad Zia',
     license='GNU General Public License v3 (GPLv3)',
